84285f7f4b0db0bdc7d368d3966e5fd87bed4e61
Manulovich <manulovicm@gmail.com>
2021-03-21 14:59:48 +0300

    input.jsx -> input.tsx and add about

--- component_library/src/App.js
+++ component_library/src/App.js
@@ -6,8 +6,8 @@ import plug from './plug.png';
 function App() {
   return (
     <div className="app">
-      <Input label="Имя:" options="Можт быть не ваше" dataIsValid="true" placeholder="Введите имя" />
-      <Input options="Пароль сюда. Быстро!" dataIsValid="true" type="password" placeholder="Введите пароль" />
+      <Input label="Имя:" options="Можт быть не ваше" placeholder="Введите имя" />
+      <Input options="Пароль сюда. Быстро!" type="password" placeholder="Введите пароль" />
       <Button type="button-danger" icon={plug} content="submit" />
     </div>
   );
--- component_library/src/index.js
+++ component_library/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './_reset.css';
+import './_reset.scss';
 import App from './App';
 
 ReactDOM.render(
